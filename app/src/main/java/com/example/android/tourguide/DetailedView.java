5f55805495aa9fa0e60498c20f72919433f53f66
LOGGERKID <revanthms.06@gmail.com>
2018-07-21 22:25:15 +0530

    Stage one implimentation of onitemClickListener to Recycler View

--- app/src/main/java/com/example/android/tourguide/DetailedView.java
+++ app/src/main/java/com/example/android/tourguide/DetailedView.java
@@ -1,7 +1,13 @@
 package com.example.android.tourguide;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class DetailedView extends AppCompatActivity {
 
@@ -9,5 +15,28 @@ public class DetailedView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailed_view);
+
+        ImageView mainImage = findViewById(R.id.detailedImage);
+        ImageView phoneImage = findViewById(R.id.detailedPhone);
+        ImageView locationImage = findViewById(R.id.detailedLocation);
+        TextView ratingText = findViewById(R.id.detailedRating);
+        ArrayList<Places> receivedPlacesToDisplay = new ArrayList<Places>();
+        //receivedPlacesToDisplay =getIntent().getExtras("Details");
+
+        phoneImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        locationImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
+
 }
--- app/src/main/java/com/example/android/tourguide/RecycleAdapter.java
+++ app/src/main/java/com/example/android/tourguide/RecycleAdapter.java
@@ -1,7 +1,11 @@
 package com.example.android.tourguide;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,13 +13,14 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RecycleAdapter extends RecyclerView.Adapter {
     private Context mContext;
-    private List<Places> placesList;
+    private ArrayList<Places> placesList;
 
-    public RecycleAdapter(Context mContext, List<Places> placesList) {
+    public RecycleAdapter(Context mContext, ArrayList<Places> placesList) {
         this.mContext = mContext;
         this.placesList = placesList;
     }
@@ -26,7 +31,35 @@ public class RecycleAdapter extends RecyclerView.Adapter {
 
         View itemsView = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_items, parent, false);
 
-        return new MyViewHolder(itemsView);
+        return new MyViewHolder(itemsView, mContext, placesList);
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public ImageView placeImage;
+        public TextView placeHeading;
+        public Context mContext;
+        public ArrayList<Places> placesList;
+
+        public MyViewHolder(View itemView, Context mContext, ArrayList<Places> placesList) {
+            super(itemView);
+            placeImage = itemView.findViewById(R.id.card_image_thumbnail);
+            placeHeading = itemView.findViewById(R.id.card_text_heading);
+            this.mContext = mContext;
+            this.placesList = placesList;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            Bundle data = new Bundle();
+            data.putSerializable("Places", placesList);
+
+
+            Intent intent = new Intent(mContext, DetailedView.class);
+            intent.putExtra("Details", this.placesList.get(getAdapterPosition()));
+            this.mContext.startActivity(intent);
+
+        }
     }
 
     @Override
@@ -42,14 +75,5 @@ public class RecycleAdapter extends RecyclerView.Adapter {
         return placesList.size();
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
-        public ImageView placeImage;
-        public TextView placeHeading;
 
-        public MyViewHolder(View itemView) {
-            super(itemView);
-            placeImage = itemView.findViewById(R.id.card_image_thumbnail);
-            placeHeading = itemView.findViewById(R.id.card_text_heading);
-        }
-    }
 }
