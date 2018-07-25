3ec2796a13155731afce3668c482e3638ad3b768
LOGGERKID <revanthms.06@gmail.com>
2018-07-25 23:46:32 +0530

    updated CategoryAdapter proper positioning
    Signed-off-by: LOGGERKID <revanthms.06@gmail.com>

--- app/src/main/java/com/example/android/tourguide/HeritageFragment.java
+++ app/src/main/java/com/example/android/tourguide/HeritageFragment.java
@@ -3,10 +3,17 @@ package com.example.android.tourguide;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 public class HeritageFragment extends Fragment {
     public HeritageFragment() {
@@ -35,3 +42,4 @@ public class HeritageFragment extends Fragment {
 
         return rootView;
     }
+}
--- app/src/main/java/com/example/android/tourguide/MuseumFragment.java
+++ app/src/main/java/com/example/android/tourguide/MuseumFragment.java
@@ -2,11 +2,18 @@ package com.example.android.tourguide;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -42,3 +49,4 @@ public class MuseumFragment extends Fragment {
         return rootView;
 
     }
+}
